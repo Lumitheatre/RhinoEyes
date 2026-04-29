2e1108442f870b2a7c95f2560da89e05cce96aa8
Phillip Dykman <phil.d324@gmail.com>
2026-04-29 02:07:23 -0700

    Link eye manifest into godot

---
+++ src/EyeManifest.gd
@@ -0,0 +1,34 @@
+@tool
+extends Resource
+class_name EyeManifest
+
+@export_file("*.cfg") var config_path: String:
+	set(val):
+		config_path = val
+		_load_manifest()
+
+var config = ConfigFile.new()
+var actor_names: Array = []
+
+func _load_manifest():
+	if config.load(config_path) == OK:
+		actor_names = config.get_sections()
+		# Notify any listening EyeNodes that the data has changed
+		emit_changed()
+
+func get_actor_list() -> String:
+	if actor_names.is_empty(): _load_manifest()
+	return ",".join(actor_names)
+
+func find_clip(actor_name: String, angle: float, aggression: int) -> String:
+	var clips = config.get_value(actor_name, "clips", [])
+	var best_path: String = ""
+	var min_diff: float = 181.0
+
+	for clip in clips:
+		if int(clip["aggression"]) == aggression:
+			var diff = abs(fmod(clip["angle"] - angle + 180, 360) - 180)
+			if diff < min_diff:
+				min_diff = diff
+				best_path = clip["path"]
+	return best_path
